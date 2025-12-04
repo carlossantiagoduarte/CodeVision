946726b278bb6cd5a698f41bc43160ccb93f462c
Nyx <blum7490@gmail.com>
2025-12-04 07:40:34 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\PermissionsSeeder; 
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
+            PermissionsSeeder::class,
             CategorySeeder::class,
             EventSeeder::class,
         ]);
